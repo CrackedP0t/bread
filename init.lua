07a389f63f9dc6eaeb28ffbb9bd32b7890436075
Elaina Martineau <elainamartineau@gmail.com>
2016-12-31 18:30:29 -0700

    Initial commit - no bugs that I can tell

---
+++ init.lua
@@ -0,0 +1,297 @@
+local titter = require("titter")
+
+local bread = {}
+
+bread.opts = {
+	side = "r",
+	xPad = 20,
+	yPad = 1,
+	width = "40%",
+	dispNumberKeys = false,
+	scrollMult = 1,
+	scrollInvert = false
+}
+bread.theme = {
+	colors = {
+		back = {150, 150, 150, 255},
+		text = {255, 255, 255, 255},
+		highlight = {200, 200, 200, 255}
+	},
+	font = love.graphics.getFont()
+}
+
+bread.watching = setmetatable({}, {__tostring = function() return "Bread" end})
+bread.minimized = {}
+bread.names = {}
+
+bread.scrollOffset = 0
+
+bread.isBinary = function(v)
+	return type(v) == "thread" or type(v) == "userdata" or type(v) == "function"
+end
+
+bread.validID = function(k)
+	if type(k) ~= "string" then
+		return false
+	else
+		return k:match("[%a_]+[%a%d_]*")
+	end
+end
+
+bread.iter = function()
+	return titter(bread.watching, "Bread")
+end
+
+bread.getRowHeight = function()
+	return bread.theme.font:getHeight() + bread.opts.yPad
+end
+
+bread.getTextHeight = function()
+	local minimizeLevel = 0
+	local yDepth = 0
+	for f, k, v, l in bread.iter() do
+		if minimizeLevel == 0 then
+			yDepth = yDepth + 1
+
+			if bread.minimized[v] then
+				minimizeLevel = 1
+			end
+		else
+			if f then
+				minimizeLevel = minimizeLevel - 1
+			elseif type(v) == "table" then
+				minimizeLevel = minimizeLevel + 1
+			end
+		end
+	end
+
+	return yDepth * bread.getRowHeight()
+end
+
+bread.pointInPanel = function(x, y)
+	local width = bread.getBarWidth()
+	return (bread.opts.side == "l" and x < width) or (bread.opts.side == "r" and x > love.graphics.getWidth() - width)
+end
+
+bread.screenToDepth = function(y)
+	local index = math.floor((y - bread.scrollOffset) /  bread.getRowHeight())
+	return index
+end
+
+bread.getBarWidth = function()
+	local width
+	if type(bread.opts.width) == "string" and bread.opts.width:find("%%") then
+		width = love.graphics.getWidth() * tonumber(bread.opts.width:gsub("%%", "") .. "") / 100
+	else
+		width = tonumber(bread.opts.width)
+	end
+	return width
+end
+
+bread.getCoord = function(x, y)
+	return x * bread.opts.xPad, y * bread.getRowHeight()
+end
+
+bread.drawText = function(text, x, y)
+	love.graphics.push("all")
+	love.graphics.setFont(bread.theme.font)
+	love.graphics.setColor(bread.theme.colors.text)
+	love.graphics.print(text, bread.getCoord(x, y))
+	love.graphics.pop()
+end
+
+bread.watch = function(thing, name)
+	assert(type(thing) == "table")
+
+	bread.watching[#bread.watching + 1] = thing
+	bread.names[thing] = name
+
+	return thing
+end
+
+bread.doMinimize = function(x, y)
+	local minimizeLevel = 0
+	local tDepth = bread.screenToDepth(y)
+	local yDepth = 0
+	local alreadyEnded = false
+	for f, k, v, l, e in bread.iter() do
+		if minimizeLevel == 0 then
+			if yDepth == tDepth then
+				if type(v) == "table" and not e then
+					bread.minimized[v] = not bread.minimized[v]
+				end
+
+				break
+			end
+
+			if not alreadyEnded then
+				yDepth = yDepth + 1
+			end
+
+			alreadyEnded = false
+
+			if e then
+				alreadyEnded = true
+			elseif bread.minimized[v] then
+				minimizeLevel = 1
+			end
+		else
+			if f then
+				minimizeLevel = minimizeLevel - 1
+			elseif type(v) == "table" then
+				minimizeLevel = minimizeLevel + 1
+			end
+		end
+	end
+end
+
+bread.mousepressed = function(x, y, b)
+	local pointValid = bread.pointInPanel(x, y)
+	if pointValid then
+		if b == 1 then
+			bread.doMinimize(x, y)
+		end
+	end
+	return pointValid
+end
+
+bread.wheelmoved = function(x, y)
+	local pointValid = bread.pointInPanel(love.mouse.getX(), love.mouse.getY())
+	if pointValid and y ~= 0 then
+		local textHeight = bread.getTextHeight()
+		local loveHeight = love.graphics.getHeight()
+		local rowHeight = bread.getRowHeight()
+
+		local desiredScroll = bread.scrollOffset
+			- (math.abs(y) / y) * bread.opts.scrollMult * rowHeight
+		if desiredScroll > loveHeight - textHeight and desiredScroll <= 0 then
+			bread.scrollOffset = desiredScroll
+		end
+	end
+	return pointValid
+end
+
+bread.draw = function()
+	local size = bread.getBarWidth()
+	local drawRect = function()
+		if not bread.minimized[bread.watching] then
+			love.graphics.rectangle("fill", 0, 0, size, love.graphics.getHeight())
+		else
+			love.graphics.rectangle("fill", 0, 0, size, bread.getRowHeight())
+		end
+	end
+
+	love.graphics.push("all")
+
+	if bread.opts.side == "r" then
+		love.graphics.translate(love.graphics.getWidth() - size, 0)
+	end
+
+	love.graphics.push("all")
+
+	love.graphics.setColor(bread.theme.colors.back)
+	drawRect()
+	love.graphics.pop()
+
+	love.graphics.stencil(drawRect)
+	love.graphics.setStencilTest("greater", 0)
+
+	love.graphics.translate(0, bread.scrollOffset)
+
+	local yDepth = 0
+	local xDepth = 0
+	local minimizeLevel = 0
+	local alreadyEnded = false
+	for finish, key, value, last, empty in bread.iter() do
+		local minimized = bread.minimized[value]
+
+		if minimizeLevel == 0 then
+			if finish then
+				last = key
+				xDepth = xDepth - 1
+				if not alreadyEnded then
+					bread.drawText("}" .. (last and "" or ","), xDepth, yDepth)
+				else
+					alreadyEnded = false
+				end
+			else
+				if bread.pointInPanel(love.mouse.getX())
+				and bread.screenToDepth(love.mouse.getY()) == yDepth then
+					local rowHeight = bread.getRowHeight()
+
+					love.graphics.push("all")
+					love.graphics.setColor(bread.theme.colors.highlight)
+					love.graphics.rectangle("fill", 0, yDepth * rowHeight, size, rowHeight)
+					love.graphics.pop()
+				end
+
+				local dispKey = true
+				local name
+				if bread.names[value] then
+					name = "*" .. bread.names[value] .. "*"
+				else
+					dispKey = type(key) ~= "number" or bread.opts.dispNumberKeys
+					name = tostring(key)
+
+					if type(key) ~= "string" or not bread.validID(name) then
+						if type(key) == "string" then
+							name = "\"" .. name .. "\""
+						end
+						if type(key) == "table" or bread.isBinary(v) then
+							name = "<" .. name .. ">"
+						end
+						name = "[" .. name .. "]"
+					end
+				end
+
+				local strung
+				if type(value) == "table" then
+					strung = "{"
+					if minimized then
+						strung = strung .. "...}"
+					elseif empty then
+						strung = strung .. "}"
+						alreadyEnded = true
+					end
+					if (minimized or empty) and not last then
+						strung = strung .. ","
+					end
+				else
+					strung = tostring(value)
+
+					if type(value) == "string" then
+						strung = "\"" .. strung .. "\""
+					elseif bread.isBinary(value) then
+						strung = "<" .. strung .. ">"
+					end
+
+					if not last then
+						strung = strung .. ","
+					end
+				end
+
+				bread.drawText((dispKey and name .. " = " or "") .. strung, xDepth, yDepth)
+
+				if minimized then
+					minimizeLevel = 1
+				elseif type(value) == "table" then
+					xDepth = xDepth + 1
+				end
+			end
+
+			if not alreadyEnded then
+				yDepth = yDepth + 1
+			end
+		else
+			if finish then
+				minimizeLevel = minimizeLevel - 1
+			elseif type(value) == "table" then
+				minimizeLevel = minimizeLevel + 1
+			end
+		end
+	end
+
+	love.graphics.pop()
+end
+
+return bread
---
+++ main.lua
@@ -0,0 +1,35 @@
+local bread = require("init") -- This is because the Bread library is in this folder.
+-- Change this in your projects!
+
+local static = {
+	t1 = {["1 *"] = "asdf", "asdfg", a = 1},
+	{t = {"a", "b", {}}},
+	{{{"hi"}}},
+	[coroutine.create(function()end)] = 5
+}
+
+local keys = {}
+local presses = {}
+
+bread.watch(static, "Static")
+bread.watch(keys, "Keys")
+bread.watch(presses, "Presses")
+
+function love.draw()
+	bread.draw()
+end
+
+function love.mousepressed(x, y, b)
+	if not bread.mousepressed(x, y, b) then
+		table.insert(presses, 1, {x = x, y = y, b = b})
+		presses[11] = nil
+	end
+end
+
+function love.wheelmoved(x, y)
+	bread.wheelmoved(x, y)
+end
+
+function love.keypressed(k)
+	keys[k] = keys[k] and keys[k] + 1 or 1
+end
